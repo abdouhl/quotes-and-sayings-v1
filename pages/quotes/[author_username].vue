ac8dd683d8794ac46079ddc20df6192367dffa5f
abdou_hl <abderrahmane.36.hellal@gmail.com>
2023-02-10 17:39:35 +0100

    fix a tag

--- pages/quotes/[author_username].vue
+++ pages/quotes/[author_username].vue
@@ -107,12 +107,12 @@ useHead({
   <div v-else>
   	<header class="items-center bg-white shadow border-t-4 border-slate-800">
   <div class="flex flex-1 items-center p-3">
-    <NuxtLink class="flex items-center font-mono tracking-tighter" :to="localePath('/')">
+    <a class="flex items-center font-mono tracking-tighter" :href="localePath('/')">
       <svg class="w-10 mr-2" viewBox="0 0 121.5 114.7" xmlns="http://www.w3.org/2000/svg">
         <path d="m 0.5 65 c -5 -62.5 77.5 -92.5 110 -30 h -17.5 c -7.5 -20 -62.5 -37.5 -76.25 15 h 40 l 65 65 h -21.25 l -10 -10 c -35 22.5 -75 5 -85 -25 h 17.5 c 7.5 15 30 27.5 55.75 13.75 l -28.25 -28.75 z m 70 -15 h 43.5 c 1.5 5 1.5 20 -7 36.5 z" fill="currentFill"/>
       </svg>
       <pre class="font-mono font-bold text-sm">{{ $t('title') }}</pre>
-    </NuxtLink>
+    </a>
   </div>
   <nav class="z-50 absolute top-0 right-0 mt-3 md:w-auto py-1 px-2">
     <ul class="flex justify-end items-start text-slate-900 text-sm text-center">
@@ -120,7 +120,7 @@ useHead({
         <div class=" w-16 md:w-24">
           <a href="#" class="block py-1 px-1" @click.prevent="nav.lang=!nav.lang">{{locales.map(i=>i.name)[locales.map(i=>i.code).indexOf(locale)]}}</a>
           <ul v-if="nav.lang" class="my-2 text-sm leading-loose text-center">
-						<li v-for="locale in availableLocales" ><NuxtLink :key="locale.code" :to="switchLocalePath(locale.code)">{{locale.name}}</NuxtLink></li>
+						<li v-for="locale in availableLocales" ><a :key="locale.code" :href="switchLocalePath(locale.code)">{{locale.name}}</a></li>
 						</ul>
         </div>
       </li>
